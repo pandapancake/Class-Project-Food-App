e180a2de5fc06485e041a16b104d514b11441b87
benny <benny@squarevibe.com>
2018-06-02 10:32:00 -0400

    removed Pods

--- Pods/Target Support Files/Pods-Simple Ingredient/Pods-Simple Ingredient-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Simple_Ingredient : NSObject
-@end
-@implementation PodsDummy_Pods_Simple_Ingredient
-@end
--- Pods/Target Support Files/Pods-Simple IngredientTests/Pods-Simple IngredientTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Simple_IngredientTests : NSObject
-@end
-@implementation PodsDummy_Pods_Simple_IngredientTests
-@end
